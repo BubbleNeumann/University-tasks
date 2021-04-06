622ce8977726a64366d39b5b1bbc370ea7e538c7
BubbleNeumann <dana@chkgo.com>
2021-04-06 20:14:31 +0300

    Passes all tests available. Comments added

--- sem2_BoPLT_lab_3.cpp
+++ sem2_BoPLT_lab_3.cpp
@@ -7,25 +7,28 @@
  Automata states
  S - start (first symbol)
  A - correct letter found (possible kw or id)
- B - digit found
+ B - digit found (vl lexeme type)
  E - error (wl)
  H - correct exit (got the string of special chracters)
  F - correct exit (possible kw or id)
-
+ HsA - previous correct "word" lexeme, now delimiter character found
+ HsB - previous correct "const" lexeme, now delimiter character found
  */
-enum AState { S, A, B, C, E, F, G, H, Ef, HsA, HsB, HsC };
+enum AState { S, A, B, C, E, M , F, G, H, Ef, Hs, HsA, HsB, HsC};
 
 const char lex_name[8][3]{ "kw", "vl", "co", "ao", "id", "eq", "sc", "wl" };
 enum LexType { kw, vl, co, ao, id, eq, sc, wl };
 
-const AState matrix[4][5]
+
+const AState matrix[6][6]
 {
-    // S, A, B, C, E
-      {A, A, E,HsC, E}, // alpha
-      {B, E, B,HsC, E}, // digit
-      //{C,Hs,Hs,Hs,Hs}, // special character, which is a separator
-      {C,HsA,HsB, C, E}, // special character
-      {S, F, G, H, Ef} // space, tab, EOL
+    // S, A, B, C, E, M
+      {A, A, E,HsC, E, HsC},    // alpha
+      {B, E, B,HsC, E, HsC},    // digit    
+      {C,E,E, C, E, HsC},       // special character
+      {S, F, G, H, Ef, H},      // space, tab, EOL
+      {Hs,HsA,HsB,HsC,Ef, HsC}, // special character, which is a separator
+      {M, HsA, HsB, HsC, Ef, M} // special separator which could be paired
 };
 
 int getPath(char curr, char first)
@@ -33,7 +36,8 @@ int getPath(char curr, char first)
     if (isspace(curr) || curr == '\t' || curr == '\0' || curr == '\n') return 3;
     if ((curr >= 'a' && curr <= 'z') || (curr >= 'A' && curr <= 'Z')) return 0;
     if (isdigit(curr)) return 1;
-    //if (curr == '>' || curr == '=' || curr == '<' || curr == '+') return 2;
+    if (curr == '+' || curr == '-' || curr == '*' || curr == '/' || curr == '=' || curr == ';') return 4;
+    if (curr == '>' || curr == '<') return 5;
 
     return 2;
 }
@@ -73,35 +77,19 @@ int coType(const Lex& a)
 void addLex(std::vector<Lex>& result, AState& state, char* lexeme)
 {
     Lex lex;
-
     lex.body = lexeme;
 
-    if (state == F)
+    if (state == F || state == HsA)
     {
         lex.type = (LexType)wordType(lex);
     }
 
-    if (state == G)
+    if (state == G || state == HsB)
     {
         lex.type = vl;
     }
 
-    if (state == H)
-    {
-        lex.type = (LexType)coType(lex);
-    }
-
-    if (state == HsA)
-    {
-        lex.type = (LexType)wordType(lex);
-    }
-
-    if (state == HsB)
-    {
-        lex.type = vl;
-    }
-
-    if (state == HsC)
+    if (state == H || state == HsC || state == Hs)
     {
         lex.type = (LexType)coType(lex);
     }
@@ -114,7 +102,7 @@ void addLex(std::vector<Lex>& result, AState& state, char* lexeme)
     state = S;
 }
 
-void newLexAnalysis(std::vector<Lex>& result, char*& str)
+void lexAnalysis(std::vector<Lex>& result, char*& str)
 {
     int pos = 0; // current position
     int firstPos; // lexeme beginning
@@ -126,13 +114,12 @@ void newLexAnalysis(std::vector<Lex>& result, char*& str)
         {
             firstPos = pos;
         }
-        // walk through matrix states
 
+        // walk through matrix states
         state = matrix[getPath(str[pos], str[firstPos])][state];
 
-        // remember the lexeme
-
-        if (state == F || state == G || state == H || state == HsA || state == HsB || state == HsC || state == Ef)
+        // remember the lexeme if end state is reached
+        if (state == F || state == G || state == H || state == HsA || state == HsB || state == HsC || state == Ef )
         {
             char* lexeme = new char[pos - firstPos + 1];
             int t1 = firstPos;
@@ -141,13 +128,25 @@ void newLexAnalysis(std::vector<Lex>& result, char*& str)
                 lexeme[i] = str[t1];
             }
             lexeme[pos - firstPos] = '\0';
+
+            // if needed to start a new lexeme with a current symbol
             if (state == HsA || state == HsB || state == HsC) --pos;
+            addLex(result, state, lexeme);
+        }
+
+        // automata state which is needed to write down just the current symbol
+        if (state == Hs)
+        {
+            char* lexeme = new char[2];
+            lexeme[0] = str[pos];
+            lexeme[1] = '\0';
             addLex(result, state, lexeme);
         }
 
     } while (str[pos++] != '\0');
 }
 
+// read the .txt file content 
 char* getSymbols()
 {
     std::ifstream fromf("input.txt", std::ios::binary);
@@ -178,10 +177,13 @@ void print(std::vector<Lex>& v)
 {
     std::ofstream inf("output.txt");
 
+    // print the first row
     for (size_t i = 0; i < v.size(); ++i) {
         inf << v[i].body << '[' << lex_name[v[i].type] << ']' << ' ';
     }
     inf << '\n';
+
+    // the second row
     for (size_t i = 0; i < v.size(); ++i) {
         if (lex_name[v[i].type] == lex_name[4])
         {
@@ -190,6 +192,8 @@ void print(std::vector<Lex>& v)
         }
     }
     inf << '\n';
+
+    //the third row
     for (size_t i = 0; i < v.size(); ++i) {
         if (lex_name[v[i].type] == lex_name[1])
         {
@@ -203,7 +207,7 @@ int main()
 {
     char* str = getSymbols();
     std::vector<Lex> list;
-    newLexAnalysis(list, str);
+    lexAnalysis(list, str);
     print(list);
 
     delete[] str;
