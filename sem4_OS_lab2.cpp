f6e69a8d2a8335ee814662275de1aeafeb580d98
BubbleNeumann <dana@chkgo.com>
2022-02-22 21:58:44 +0400

    getFileInfo() added

--- sem4_OS_lab2.cpp
+++ sem4_OS_lab2.cpp
@@ -1,162 +1,73 @@
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <iostream>
+#include <time.h>
 
-// for Unix-like systems only
-//#include <unistd.h>
 
-
-
-int umask(int newmask)
+void showHelp()
 {
-	
+    std::cout << "cp SOURCE DEST\n" << "mv SOURCE DEST\n" 
+        << "stat FILE\n"<< "chmod MODE FILE" << std::endl;
 }
 
-/**
- * @brief 
- * @param pathname 
- * @param flags 
- * @param mode_t Permission bits
- * @return 
-*/
-//int open(char* pathname, int flags, mode_t mode)
-//{
-//
-//}
 
-
-int close(int fd)
+void copy(std::string source, std::string dest)
 {
 
 }
 
-
-size_t read(int fd, void* buf, size_t length)
+void move(std::string source, std::string dest)
 {
 
 }
 
+void getFileInfo(std::string filename = "sem4_OS_lab2.cpp")
+{
+    // convert std::string to const char*
+    const char* chfilename = filename.c_str();
 
-size_t read(int fd, const void* buf, size_t length)
+    struct stat st;
+    stat(chfilename, &st);
+
+    // size of given file
+    std::cout << st.st_size << " bytes" << std::endl;
+
+    // file permissions
+    const int MOD_LEN = 9;
+    char* mod = new char[MOD_LEN];
+    mode_t perm = st.st_mode;
+    mod[0] = (perm & S_IRUSR) ? 'r' : '-';
+    mod[1] = (perm & S_IWUSR) ? 'w' : '-';
+    mod[2] = (perm & S_IXUSR) ? 'x' : '-';
+    mod[3] = (perm & S_IRGRP) ? 'r' : '-';
+    mod[4] = (perm & S_IWGRP) ? 'w' : '-';
+    mod[5] = (perm & S_IXGRP) ? 'x' : '-';
+    mod[6] = (perm & S_IROTH) ? 'r' : '-';
+    mod[7] = (perm & S_IWOTH) ? 'w' : '-';
+    mod[8] = (perm & S_IXOTH) ? 'x' : '-';
+
+    std::cout << std::string(mod, MOD_LEN) << std::endl;
+
+    // last modified time 
+    char buff[100];
+    time_t time = st.st_ctime;
+    strftime(buff, 100, "%Y-%m-%d %H:%M:%S", localtime (&time));
+    std::cout << "last edit: " << buff << std::endl;
+}
+
+void changeFilePermissions(std::string mode, std::string filename)
 {
 
 }
-
-size_t write(int fd, void* buf, size_t length)
-{
-
-}
-
-/**
- * @brief 
- * @param fd 
- * @param buf 
- * @param length 
- * @return 
-*/
-size_t write(int fd, const void* buf, size_t length)
-{
-
-
-
-
-}
-
-
-/**
- * @brief Put read/write pointer
- * @param fd 
- * @param offset 
- * @param whence A place to put the pointer : SEEKSET, SEEK_CUR, SEEKEND
- * @return 
-*/
-int lseek(int fd, off_t offset, int whence)
-{
-
-}
-
-
-int truncate(const char* pathname, size_t length)
-{
-
-}
-
-
-int truncate(int fd, size_t length)
-{
-
-}
-
-int fsync(int fd)
-{
-
-}
-
-int fdatasync(int fd)
-{
-
-}
-
-/**
- * @brief 
- * @param pathname 
- * @param statbuf 
- * @return 
-*/
-int stat(const char* pathname, struct stat* statbuf)
-{
-
-}
-
-int lstat(const char* pathname, struct stat* statbuf)
-{
-
-}
-
-int fstat(int fd, struct stat* statbuf)
-{
-
-}
-
-//int chmod(const char* pathname, mode_t mode)
-//{
-//
-//}
-
-//int fchmod(int fd, mode_t mode)
-//{
-//
-//}
-
-
-int link(const char* origpath, const char* newpath)
-{
-
-}
-
-int symlink(const char* origpath, const char* newpath)
-{
-
-}
-
-int unlink(char* pathname)
-{
-
-}
-
-
-int unlink(char* pathname)
-{
-
-}
-
-int rename(const char* oldpath, const char* newpath)
-{
-
-}
-
 
 
 int main()
 {
-	return 0;
+    
+    getFileInfo();
+
+    // showHelp();
+
+    return 0;
 }
