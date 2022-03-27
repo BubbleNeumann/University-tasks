7e7d96f98f1f532fe0d05f1140e56e4eae3e18ff
BubbleNeumann <dana@chkgo.com>
2022-03-27 10:30:24 +0400

    upd

--- sem4_OS_lab3.cpp
+++ sem4_OS_lab3.cpp
@@ -5,6 +5,8 @@
 #include <cstring>
 #include <regex>
 
+int pipe_in[2];
+int pipe_out[2];
 
 struct userData
 {
@@ -13,16 +15,35 @@ struct userData
 	int numberOfSplits;
 };
 
+void showHelp(const std::string request)
+{
+	std::cout << "Given programm calculates an integral of x^2 in borders set by user.\n
+                  Now you're expected to provide " << request << std::endl;
+}
+
+double integrate(double a, double b, int n)
+{
+	double result = 0;
+	double h = (b - a) / n;
+
+	for (int x = a, int i = 0; i < n; ++i, x += h)
+	{
+		result += h * (x * x + (x + h) * (x + h)) / 2;
+	}
+
+	return result;
+}
+
 int readNum(const std::string mod)
 {
 	std::string userInp;
-    getline(std::cin, userInp);
-    const std::regex rxint{R"([0-9]$)"};
-    const std::regex rxdouble{R"([0-9.]$)"};
+    const std::regex rxint{R"(^[0-9]+$)"};
+    const std::regex rxdouble{R"(^[0-9]*(?:\.[0-9]+)?$)"};
 
     while (true)
     {
-        if (userInp == "--help" || userInp == "help")
+        getline(std::cin, userInp);
+        if (userInp == "--help" || userInp == "help" || userInp == "h")
             showHelp(mod);
         else if (mod == "int" && regex_match(userInp, rxint))
             return std::stoi(userInp);
@@ -40,7 +61,7 @@ void client()
 	data.rightBorder = readNum("double");
 	data.numberOfSplits = readNum("int");
 
-    if (data.leftBorder > data. rightBorder)
+    if (data.leftBorder > data.rightBorder)
         std::swap(data.leftBorder, data. rightBorder);
 
 	write(pipe_in[1], &data, sizeof(userData));
@@ -56,39 +77,17 @@ void server()
 {
 	userData data;
 	read(pipe_in[0], &data, sizeof(userData));
-	write(pipe_out[1], integrate(data.leftBorder, data.rightBorder, data.numberOfSplits), sizeof(double));
-}
-
-double integrate(double a, double b, int splits)
-{
-	double result = 0;
-	double lengthOfOneSplit = (b - a) / splits;
-
-	for (int x = a, int i = 0; i < splits; ++i)
-	{
-		result += lengthOfOneSplit * (x * x + (x + lengthOfOneSplit) * (x + lengthOfOneSplit)) / 2;
-		x += lengthOfOneSplit;
-	}
-
-	return result;
-}
-
-void showHelp(const std::string request)
-{
-	std::cout << "Given programm calculates an integral of x^2 in borders set by user.\n
-                  Now you're expected to provide " << request << std::endl;
+    double res = integrate(data.leftBorder, data.rightBorder, data.numberOfSplits);
+	write(pipe_out[1], &res, sizeof(double));
+    exit(0);
 }
 
 int main()
 {    
-    pid_t pid; // pid_t - data type for process id
-    int pipe_in[2];
-    int pipe_out[2];
-
     pipe(pipe_in);
 	pipe(pipe_out);
 
-    switch(pid=fork())
+    switch(fork())
     {
         case -1:
             std::cout << "can't fork the process";
