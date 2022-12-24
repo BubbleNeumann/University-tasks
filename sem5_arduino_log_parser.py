c01a8f35dbc4b91b166bb773cddfe380859dc0d2
BubbleNeumann <53640776+BubbleNeumann@users.noreply.github.com>
2022-12-24 12:34:23 +0400

    finale

--- sem5_arduino_log_parser.py
+++ sem5_arduino_log_parser.py
@@ -14,10 +14,12 @@ mean_hum = []
 with open(FILE_NAME, 'r') as file:
     attr = [line.split(' ') for line in file]
 
+# remove first day
 first_date = attr[0][0]
 while attr and attr[0][0] == first_date:
     attr.pop(0)
 
+# remove last day
 last_date = attr[-1][0]
 while attr and attr[-1][0] == last_date:
     del attr[-1]
@@ -26,7 +28,7 @@ while attr and attr[-1][0] == last_date:
 for line in attr:
     line[3] = line[3][:-1]
 
-for j in range(0, len(attr), 3):
+for j in range(0, len(attr), 24):
     dates.append(attr[j][0])
     mean_hum.append(sum(float(attr[j+i][3]) for i in range(24))/24)
     mean_temp.append(sum(float(attr[j+i][2]) for i in range(24))/24)
